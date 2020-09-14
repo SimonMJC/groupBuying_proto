c2201d144a45a519981d354e37f9b412418871df
SimonMJC <skgmlwo98@naver.com>
2020-09-14 17:24:17 +0900

    initial exam commit

--- android/app/src/main/java/com/reproto/MainActivity.java
+++ android/app/src/main/java/com/reproto/MainActivity.java
@@ -1,6 +1,13 @@
 package com.reproto;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +18,13 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "ReProto";
+  }
+
+  
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+            new MainReactPackage(),
+            new RNGoogleSigninPackage() // <-- this needs to be in the list
+    );
   }
 }
